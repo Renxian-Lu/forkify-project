54277e1e275e52e2e74c7743687af9efbc9af9fb
Renxian-Lu <xian657241904@gmail.com>
2021-10-17 18:06:53 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '339229f7-6c62-4cdb-8603-6756a000e68b';
+export const MODAL_CLOSE_SEC = 2.5;
